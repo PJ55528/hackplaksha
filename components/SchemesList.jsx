894062c47ee62cb255b359885dbc8676ba6f917f
Yuven-0 <yuven.blowria.ug23@plaksha.edu.in>
2024-02-25 13:27:57 +0530

    Your commit message

--- components/SchemesList.jsx
+++ components/SchemesList.jsx
@@ -5,9 +5,9 @@ import { HiPencilAlt } from "react-icons/hi";
 export default function SchemesList() {
     return (
         <>
-        <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+        <div className="p-4 border border-slate-300 my-3 rounded-lg flex justify-between gap-5 items-center" style={{background: 'linear-gradient(to right, rgba(255,126,95,0.7), rgba(254,180,123,0.7))', height: '150px', opacity: 0.9}}>
             <div>
-                <h2 className="font-bold text-2xl">Schemes Title</h2>
+                <h2 className="font-san francisco text-2xl">Schemes Title</h2>
                 <div>
                     Loan Amount
                     Loan Interest
